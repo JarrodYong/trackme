841c9134041d382d111be2c42784c397a6837d7f
JarrodYong <41532177+JarrodYong@users.noreply.github.com>
2019-07-18 09:33:43 +1000

    1.3

---
+++ web/public/app.js
@@ -0,0 +1,26 @@
+$('#navbar').load('navbar.html');
+const devices = JSON.parse(localStorage.getItem('devices')) || [];
+
+devices.push({ user: "Mary", name: "Mary's iPhone" });
+devices.push({ user: "Alex", name: "Alex's Surface Pro" });
+devices.push({ user: "Mary", name: "Mary's MacBook" });
+devices.forEach(function(device) {
+    $('#devices tbody').append(`
+    <tr>
+    <td>${device.user}</td>
+    <td>${device.name}</td>
+    </tr>`
+    );
+});
+localStorage.setItem('devices', JSON.stringify(devices));
+$('#add-device').on('click', function() {
+    const user = $('#user').val();
+    const name = $('#name').val();
+    devices.push({ user, name });
+    location.href= '/';
+    });
+  
+    $('#send-command').on('click', function() {
+        const command = $('#command').val();
+        console.log(`command is: ${command}`);
+        });
