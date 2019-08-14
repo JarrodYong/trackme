7fdd4664df55b07a1b38edfcf5a215bf0dbdc626
JarrodYong <41532177+JarrodYong@users.noreply.github.com>
2019-08-14 22:51:59 +1000

    Update server.js

--- web/server.js
+++ web/server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 8888;
 const base = `${__dirname}/public`;
 app.use(express.static('public'));
 app.get('/', (req, res) => {
